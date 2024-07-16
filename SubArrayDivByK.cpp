1be97c4e0abd834348f14504cb01f43ef5ae484d
Akash Pandey <72986203+akash-pandey1209@users.noreply.github.com>
2024-07-17 03:51:24 +0530

    Add files via upload

---
+++ SubArrayDivByK.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int,int> mpp;
+        mpp[0]=1;
+        int presum =0, count=0;
+        for(int i =0; i<nums.size(); i++){
+            presum += nums[i];
+            int mod = ((presum%k)+k)%k;
+            if(mpp.find(mod)!=mpp.end()){
+                count+= mpp[mod];
+            }
+            mpp[mod] += 1;
+        }
+
+
+        return count;
+
+        
+    }
+};
